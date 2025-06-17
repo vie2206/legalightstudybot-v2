4de05ad4364497ffda9b04b25d89bf346f01cbb8
vie2206 <official@legalight.in>
2025-06-17 19:10:36 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,14 @@
 # models.py
-from sqlalchemy import Column, Integer, Date
+
+from sqlalchemy import Column, Integer, Date, Boolean
 from database import Base
 
 class Checkin(Base):
     __tablename__ = "checkins"
     user_id = Column(Integer, primary_key=True)
     date    = Column(Date,    primary_key=True)
+
+class StreakAlert(Base):
+    __tablename__ = "streak_alerts"
+    user_id    = Column(Integer, primary_key=True)
+    is_enabled = Column(Boolean, default=False, nullable=False)
